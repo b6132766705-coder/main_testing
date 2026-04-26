786722ccaaadd9123210526547e13ac39426901a
b6132766705-coder <b6132766705@gmail.com>
2026-04-26 14:48:18 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-TOKEN = os.getenv("BOT_TOKEN")
-DB_PATH = os.getenv("DB_NAME")
+TOKEN = os.getenv("BOT_TOKEN") # Убедись, что токен загружается (например, через .env)
+ADMIN_ID = 1316137517
+DB_PATH = "/app/data/butya.db"
