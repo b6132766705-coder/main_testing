676d6d95dbd8a68890d3b5541cbc38e016e77435
b6132766705-coder <b6132766705@gmail.com>
2026-04-26 22:04:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
+
 from config import TOKEN
 from database.db import init_db
-from handlers import games, economy, clans # Импортируем твои папки
+from handlers import common
 
 async def main():
-    # Инициализируем базу данных
+    logging.basicConfig(level=logging.INFO)
+    
+    # Создаем таблицы в БД, если их нет
     await init_db()
-
+    
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
-
-    # Подключаем роутеры (обработчики)
-    dp.include_routers(
-        games.router,
-        economy.router,
-        clans.router
-    )
-
-    print("Бот запущен!")
+    
+    # Регистрируем наши роутеры
+    dp.include_router(common.router)
+    
+    print("🚀 Бот успешно запущен!")
+    await dp.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
