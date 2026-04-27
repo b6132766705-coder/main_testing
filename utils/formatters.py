f01e28cd1106868f728ef77df40c1962bc6a2bcb
b6132766705-coder <b6132766705@gmail.com>
2026-04-26 22:20:54 -0400

    Refactor fmt function for number formatting
    Refactor number formatting function to use format method.

--- utils/formatters.py
+++ utils/formatters.py
@@ -1,3 +1,3 @@
-def fmt(num: int) -> str:
-    """Форматирует числа, добавляя пробелы (10000 -> 10 000)"""
-    return f"{num:,}".replace(",", " ")
+def fmt(number: int) -> str:
+    return "{:,}".format(number).replace(",", " ")
+
