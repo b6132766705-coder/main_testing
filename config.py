5f4b8a4d2e2cbc794174cd2e55047cbd6c87b984
b6132766705-coder <b6132766705@gmail.com>
2026-04-26 22:19:18 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
 
-TOKEN = os.getenv("BOT_TOKEN") # Убедись, что токен загружается (например, через .env)
-ADMIN_ID = 1316137517
-DB_PATH = "/app/data/butya.db"
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+DB_PATH = "database/game_bot.db"
+
